053b0c11c26a45cd75c7c9d94934e28da111490e
Diksha Shamrao Kuthe <kuthediksha123@gmail.com>
2025-05-25 00:02:03 +0530

    update all changing files

---
+++ frontend/resume_builder/src/pages/ResumeUpdate/EditResume.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const EditResume = () => {
+    return (
+        <div>
+            <h1>Edit Resume</h1>
+            <p>Update your resume details here.</p>
+        </div>
+    );
+}
+
+export default EditResume;
